5c62e983612b14e5b4c514f595b325caf6497894
Amir Mahdian <amah664@aucklanduni.ac.nz>
2024-05-30 01:40:52 +1200

    Finished the logic for loading the map int and the logic for the info-country button just have to add in the exception to check for incorrect input

---
+++ src/main/java/nz/ac/auckland/se281/Country.java
@@ -0,0 +1,13 @@
+package nz.ac.auckland.se281;
+
+public class Country {
+  String name;
+  String continent;
+  double taxFees;
+
+  public Country(String name, String continent, double taxFees) {
+    this.name = name;
+    this.continent = continent;
+    this.taxFees = taxFees;
+  }
+}
--- src/main/java/nz/ac/auckland/se281/MapEngine.java
+++ src/main/java/nz/ac/auckland/se281/MapEngine.java
@@ -1,9 +1,14 @@
 package nz.ac.auckland.se281;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /** This class is the main entry point. */
 public class MapEngine {
+
+  private Map<String, Country> countriesMap;
 
   public MapEngine() {
     // add other code here if you want
@@ -14,12 +19,38 @@ public class MapEngine {
   private void loadMap() {
     List<String> countries = Utils.readCountries();
     List<String> adjacencies = Utils.readAdjacencies();
-    // add code here to create your data structures
+
+    Map<String, List<String>> graph = new HashMap<>(); // Declare the graph variable
+
+    countriesMap = new HashMap<>();
+
+    for (String countryInfo : countries) {
+      String[] info = countryInfo.split(",");
+      String countryName = Utils.capitalizeFirstLetterOfEachWord(info[0].toLowerCase());
+      String continent = info[1];
+      double taxFees = Double.parseDouble(info[2]);
+      Country country = new Country(countryName, continent, taxFees);
+      countriesMap.put(countryName, country);
+      graph.put(countryName, new ArrayList<>());
+    }
+
+    for (String adjacency : adjacencies) {
+      String[] adjacencyCountries = adjacency.split(",");
+      graph.get(adjacencyCountries[0]).add(adjacencyCountries[1]);
+      graph.get(adjacencyCountries[1]).add(adjacencyCountries[0]);
+    }
   }
 
   /** this method is invoked when the user run the command info-country. */
   public void showInfoCountry() {
-    // add code here
+    MessageCli.INSERT_COUNTRY.printMessage();
+    String country = Utils.capitalizeFirstLetterOfEachWord(Utils.scanner.nextLine().toLowerCase());
+
+    if (countriesMap.containsKey(country)) {
+      Country countryInfo = countriesMap.get(country);
+      MessageCli.COUNTRY_INFO.printMessage(
+          countryInfo.name, countryInfo.continent, String.valueOf(countryInfo.taxFees));
+    }
   }
 
   /** this method is invoked when the user run the command route. */
